e7c49fb9d305827185c34274dfb56594539e9cba
ariefzainuri <info@dihardjasoftware.com>
2021-02-11 15:18:30 +0700

    Implement FCM for android and ios

--- sources/lib/main.dart
+++ sources/lib/main.dart
@@ -1,16 +1,22 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:connectivity/connectivity.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_screenutil/screenutil_init.dart';
 import 'package:get/get.dart';
 import 'package:pln_flutter/utils/controller/common_controller.dart';
 import 'package:pln_flutter/utils/helper/locales_string.dart';
 import 'package:pln_flutter/utils/helper/constant.dart';
+import 'package:pln_flutter/utils/helper/text_util.dart';
 import 'package:pln_flutter/view/home/home_view.dart';
 import 'package:pln_flutter/view/login/login_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final globalNavigatorKey = GlobalKey<NavigatorState>();
+final FirebaseMessaging firebaseMessaging = FirebaseMessaging();
 final commonController = Get.put(CommonController(), permanent: true);
 
 void main() async {
@@ -28,7 +34,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  StreamSubscription _connection;
+  StreamSubscription _connection, _fcmRefreshToken;
 
   _connectivityResult(ConnectivityResult result) {
     if (result == ConnectivityResult.mobile) {
@@ -40,9 +46,103 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+  _navigateTo(String redirect) {
+    if (redirect == 'home') {
+      Get.to(HomeView());
+    }
+  }
+
+  _redirectTo(Map<String, dynamic> message) async {
+    final redirect = Platform.isAndroid ? message['data']['type'].toString() ?? '' : message['type'].toString() ?? '';
+
+    _navigateTo(redirect);
+  }
+
+  _onSelectNotification(Map<String, dynamic> message) async {
+    final redirect = Platform.isAndroid ? message['data']['type'].toString() ?? '' : message['type'].toString() ?? '';
+    final preference = await SharedPreferences.getInstance();
+    final showForegroundNotification = preference.getBool(Constant.SHOW_FOREGROUND_NOTIFICATION) ?? false;
+
+    if (showForegroundNotification) _navigateTo(redirect);
+  }
+
+  _showAndroidNotification(Map<String, dynamic> message) async {
+    var androidSetting = AndroidInitializationSettings('bisnis_koe_logo');
+    var iosSetting = IOSInitializationSettings();
+    var initializationSettings = InitializationSettings(android: androidSetting, iOS: iosSetting);
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: (_) => _onSelectNotification(message));
+
+    String title = message['notification']['title'];
+    String body = message['notification']['body'];
+
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'balelabs.flutter.core', 'balelabs.flutter.core', 'balelabs.flutter.core',
+        importance: Importance.max, priority: Priority.high);
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics, iOS: iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(TextUtil.randomInt(0, 1000), title, body, platformChannelSpecifics);
+  }
+
+  _fcmListener() {
+    firebaseMessaging.configure(
+      /// Android : For now fired when apps is in foreground
+      onMessage: (Map<String, dynamic> message) async {
+        final preference = await SharedPreferences.getInstance();
+        preference.setBool(Constant.SHOW_FOREGROUND_NOTIFICATION, false);
+        if (Platform.isAndroid) await _showAndroidNotification(message);
+        print("onMessage: $message");
+
+        // below code is use only in android platform to prevent onmessage called when notification is not clicked
+        Future.delayed(Duration(milliseconds: 500), () => preference.setBool(Constant.SHOW_FOREGROUND_NOTIFICATION, true));
+
+        return Future.value(true);
+      },
+      /// Android : Fired when notification clicked and the apps is killed
+      onLaunch: (Map<String, dynamic> message) async {
+        String title = message['notification']['title'] ?? '';
+
+        if (Platform.isAndroid && title != '') await _showAndroidNotification(message);
+        print("onLaunch: $message");
+        _redirectTo(message);
+        return Future.value(true);
+      },
+      /// Fired when notification clicked and apps is in background
+      onResume: (Map<String, dynamic> message) async {
+        String title = message['notification']['title'] ?? '';
+
+        if (Platform.isAndroid && title != '') await _showAndroidNotification(message);
+        print("onResume: $message");
+        _redirectTo(message);
+        return Future.value(true);
+      },
+    );
+  }
+
+  _initFCM() async {
+    firebaseMessaging.requestNotificationPermissions(IosNotificationSettings(sound: true, badge: true, alert: true, provisional: true));
+    await _getFcmToken();
+    _fcmListener();
+  }
+
+  _getFcmToken() async {
+    final preference = await SharedPreferences.getInstance();
+
+    firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      print("FCM_TOKEN $token");
+      preference.setString(Constant.FCM_TOKEN, token);
+    });
+    _fcmRefreshToken = firebaseMessaging.onTokenRefresh.listen((newToken) {
+      print("NEW_FCM_TOKEN $newToken");
+      if (newToken != preference.getString(Constant.FCM_TOKEN)) preference.setString(Constant.FCM_TOKEN, newToken);
+    });
+  }
+
   @override
   void dispose() {
     _connection?.cancel();
+    _fcmRefreshToken?.cancel();
 
     super.dispose();
   }
@@ -55,6 +155,8 @@ class _MyAppState extends State<MyApp> {
 
     _connection = Connectivity().onConnectivityChanged
         .listen((ConnectivityResult result) => _connectivityResult(result));
+
+    _initFCM();
   }
 
   @override
--- sources/lib/utils/helper/constant.dart
+++ sources/lib/utils/helper/constant.dart
@@ -5,6 +5,7 @@ class Constant {
   static const IS_LOGIN = 'IS_LOGIN';
   static const FCM_TOKEN = 'FCM_TOKEN';
   static const LANGUANGE_CODE = 'LANGUANGE_CODE';
+  static const SHOW_FOREGROUND_NOTIFICATION = 'SHOW_FOREGROUND_NOTIFICATION';
 
   // VARIABLE //
   static const API_PRODUCTION = 'api_production';
