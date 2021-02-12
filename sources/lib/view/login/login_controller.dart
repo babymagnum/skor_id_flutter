908a51ee532a409ac939fbacdf5a66678f368981
banu nur affan <banu.affan@gmail.com>
2021-02-12 17:44:43 +0700

    -M:implement login screen

--- sources/lib/view/login/login_controller.dart
+++ sources/lib/view/login/login_controller.dart
@@ -1,20 +1,81 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:pln_flutter/utils/helper/constant.dart';
 import 'package:pln_flutter/main.dart';
-import 'package:pln_flutter/view/home/home_view.dart';
+import 'package:pln_flutter/utils/helper/text_util.dart';
 
 class LoginController extends GetxController {
   var obsecure = true.obs;
   var loading = false.obs;
+  var email = ''.obs;
+  var password = ''.obs;
+  var autoValidate = false.obs;
+  GlobalKey<FormState> formKey = GlobalKey();
 
   setObsecure(bool value) => obsecure.value = value;
 
+  emailOnChange(String value) => email.value = value;
+
+  passwordOnChange(String value) => password.value = value;
+
+  String emailValidate(String value){
+    if(!TextUtil.validateEmail(value))
+      return 'Masukkan email dengan format benar';
+    else
+      return null;
+  }
+
+  String passwordValidate(String value){
+    if(value.length < 4)
+      return 'Masukkan password minimal 4 karater';
+    else
+      return null;
+  }
+
+  bool validateInput(){
+    if(formKey.currentState.validate()){
+      return true;
+    }else{
+      autoValidate.value = true;
+      return false;
+    }
+  }
+
+  loginWithGoogle(){
+    if(!validateInput()){
+      return;
+    }
+
+    if(commonController.notConnected.value){
+      Get.snackbar('Sorry', 'Your device not connected to the internet', 
+        snackPosition: SnackPosition.BOTTOM,
+        margin: EdgeInsets.all(16),
+        colorText: Colors.black,
+        backgroundColor: Colors.white
+      );
+      return;
+    }
+  }
+
   login() async {
+    if(!validateInput()){
+      return;
+    }
+
+    if(commonController.notConnected.value){
+      Get.snackbar('Sorry', 'Your device not connected to the internet', 
+        snackPosition: SnackPosition.BOTTOM,
+        margin: EdgeInsets.all(16),
+        colorText: Colors.black,
+        backgroundColor: Colors.white
+      );
+      return;
+    }
+
     loading.value = true;
-    await Future.delayed(Duration(seconds: 1), () {});
+    await Future.delayed(Duration(seconds: 3), () {});
     loading.value = false;
 
-    commonController.preferences.setBool(Constant.IS_LOGIN, true);
-    Get.off(HomeView());
+    // commonController.preferences.setBool(Constant.IS_LOGIN, true);
+    // Get.off(HomeView());
   }
 }
--- sources/lib/view/login/login_view.dart
+++ sources/lib/view/login/login_view.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -8,45 +11,134 @@ import 'package:pln_flutter/utils/widgets/button_loading.dart';
 import 'package:pln_flutter/view/base_view.dart';
 import 'package:pln_flutter/view/login/login_controller.dart';
 
-class LoginView extends StatefulWidget {
-  @override
-  _LoginViewState createState() => _LoginViewState();
-}
-
-class _LoginViewState extends State<LoginView> {
-
-  var _loginCt = Get.put(LoginController());
+class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var _loginCt = Get.put(LoginController());
+
     return BaseView(
-      child: Column(
+      child: Stack(
         children: [
-          Container(
-            color: GetPlatform.isAndroid ? Colors.black : Colors.transparent, height: MediaQuery.of(context).padding.top,
-            width: Get.width,
+          Positioned.fill(
+            child: CachedNetworkImage(
+              imageUrl: 'https://c0.anyrgb.com/images/836/953/office-people-business-team-laptop-working-work-human-person-computer.jpg', 
+              fit: BoxFit.cover
+            )
           ),
-          Expanded(
-            child: CupertinoScrollbar(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 24.w),
-                  child: Column(
-                    children: [
-                      SizedBox(height: 40.h,),
-                      ButtonLoading(
-                        backgroundColor: ThemeColor.primary,
-                        disable: false,
-                        title: 'Sign In',
-                        loading: false,
-                        onTap: () => _loginCt.login(),
-                        verticalPadding: 14.h,
-                        textStyle: ThemeTextStyle.biryaniBold.apply(color: Colors.white, fontSizeDelta: 14.ssp),
-                      )
-                    ],
+          Positioned.fill(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    child: CachedNetworkImage(
+                      imageUrl: 'https://balelabs.id/wp-content/uploads/2020/02/balelabs-logo.png', 
+                      fit: BoxFit.contain,
+                      width: Get.size.width/1.5,
+                    ),
                   ),
                 ),
-              ),
+                SizedBox(height: 40.h),
+                Expanded(
+                  flex: 6,
+                  child: ClipRect(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(0, 0, 0, 0.35), 
+                      ),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                        child: Obx(() {
+                          return SingleChildScrollView(
+                            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+                            child: Form(
+                              key: _loginCt.formKey,
+                              autovalidateMode: _loginCt.autoValidate.value ? AutovalidateMode.always : AutovalidateMode.disabled,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Login'.tr, style: ThemeTextStyle.biryaniBold.apply(fontSizeDelta: 28, color: Colors.white)),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 16),
+                                    child: Text('Email'.tr, style: ThemeTextStyle.biryaniBold.apply(fontSizeDelta: 14, color: Colors.white)),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 8),
+                                    child: TextFormField(
+                                      onChanged: _loginCt.emailOnChange,
+                                      validator: _loginCt.emailValidate,
+                                      keyboardType: TextInputType.emailAddress,
+                                      textInputAction: TextInputAction.next,
+                                      decoration: InputDecoration(
+                                        contentPadding: EdgeInsets.fromLTRB(12, 18, 12, 18),
+                                        filled: true,
+                                        fillColor: Colors.white.withOpacity(0.8),
+                                        hintText: 'Masukkan email anda',
+                                        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black.withOpacity(0.7)), borderRadius: BorderRadius.all(Radius.circular(8))),
+                                        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black.withOpacity(0.7)), borderRadius: BorderRadius.all(Radius.circular(8))),
+                                        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black.withOpacity(0.7)), borderRadius: BorderRadius.all(Radius.circular(8))),
+                                      ),
+                                    )
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 14),
+                                    child: Text('Password'.tr, style: ThemeTextStyle.biryaniBold.apply(fontSizeDelta: 14, color: Colors.white)),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 8),
+                                    child: TextFormField(
+                                      onChanged: _loginCt.passwordOnChange,
+                                      validator: _loginCt.passwordValidate,
+                                      keyboardType: TextInputType.visiblePassword,
+                                      textInputAction: TextInputAction.done,
+                                      obscureText: _loginCt.obsecure.value,
+                                      decoration: InputDecoration(
+                                        contentPadding: EdgeInsets.fromLTRB(12, 18, 12, 18),
+                                        filled: true,
+                                        fillColor: Colors.white.withOpacity(0.8),
+                                        hintText: 'Masukkan password anda',
+                                        suffixIcon: IconButton(
+                                          icon:  Icon(_loginCt.obsecure.value ? Icons.visibility : Icons.visibility_off, color: Colors.black), 
+                                          onPressed: () => _loginCt.setObsecure(!_loginCt.obsecure.value)
+                                        ),
+                                        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black.withOpacity(0.7)), borderRadius: BorderRadius.all(Radius.circular(8))),
+                                        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black.withOpacity(0.7)), borderRadius: BorderRadius.all(Radius.circular(8))),
+                                        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black.withOpacity(0.7)), borderRadius: BorderRadius.all(Radius.circular(8))),
+                                      ),
+                                    )
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 24),
+                                    child: ButtonLoading(
+                                      backgroundColor: ThemeColor.primary,
+                                      disable: _loginCt.loading.value,
+                                      title: 'Masuk',
+                                      loading: _loginCt.loading.value,
+                                      onTap: () => _loginCt.login(),
+                                      verticalPadding: 8.h,
+                                      textStyle: ThemeTextStyle.biryaniBold.apply(color: Colors.white, fontSizeDelta: 14.ssp),
+                                    ),
+                                  ),
+                                  Container(
+                                    alignment: Alignment.center,
+                                    padding: EdgeInsets.only(top: 14),
+                                    child: GestureDetector(
+                                      onTap: _loginCt.loginWithGoogle,
+                                      child: Text('Atau masuk dengan ', style: ThemeTextStyle.biryaniRegular.apply(fontSizeDelta: 14, color: Colors.white))
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                        );
+                        })
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
         ],
